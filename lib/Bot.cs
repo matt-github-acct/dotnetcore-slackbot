69d28e344ac63c379010fb6e2e57b45517fed0eb
Matt Baker <baker.mattc@gmail.com>
2017-03-26 22:04:30 -0600

    capture raw message as text

--- lib/Bot.cs
+++ lib/Bot.cs
@@ -9,10 +9,8 @@ namespace Slackbot
         public string Text;
         public string Channel;
         public string[] MentionedUsers = new string[0];
+        public string RawMessage;
         public string User;
-        
-        [Newtonsoft.Json.JsonExtensionData]
-        public IDictionary<string, object> ExtensionData { get; } = new Dictionary<string, object>();
     }
 
     class SlackData
@@ -79,6 +77,8 @@ namespace Slackbot
                 }
 
                 args.User = await Slack.GetUsername(this.Token, args.User);
+
+                args.RawMessage = data;
 
                 OnMessage.Invoke(this, args);
             }
