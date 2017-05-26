2df5edcdf09120ce3432e2c676b04dd15c7dab8d
Matt Baker <baker.mattc@gmail.com>
2017-05-26 17:18:50 -0600

    handle unexpected data from slack http call

--- tests/NewSlackTests.cs
+++ tests/NewSlackTests.cs
@@ -2,6 +2,7 @@ using Xunit;
 using Slackbot;
 using Moq;
 using System;
+using System.Threading.Tasks;
 
 public class NewSlackTests
 {
@@ -15,6 +16,21 @@ public class NewSlackTests
             mockHttp
                 .Setup(http => http.Get(It.IsAny<string>()))
                 .Throws(new Exception());
+
+            var sut = new NewSlack(mockHttp.Object);
+
+            var actual = await sut.GetUsername("", "");
+            Assert.Equal("", actual);
+        }
+
+        [Fact]
+        public async void when_http_client_returns_non_json()
+        {
+            var mockHttp = new Mock<IHttp>();
+
+            mockHttp
+                .Setup(http => http.Get(It.IsAny<string>()))
+                .Returns(Task.FromResult<HttpGetResult>(new HttpGetResult { Body = "not-json" }));
 
             var sut = new NewSlack(mockHttp.Object);
 
